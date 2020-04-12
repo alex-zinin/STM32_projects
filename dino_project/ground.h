875c3d976f60d6561897c547d8045e15f5524e49
alex-zinin <43748062+alex-zinin@users.noreply.github.com>
2020-04-12 13:09:05 +0300

    Create ground.h

---
+++ dino_project/ground.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "stm32f0xx_ll_rcc.h"
+#include "stm32f0xx_ll_system.h"
+#include "stm32f0xx_ll_bus.h"
+#include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_tim.h"
+#include "stm32f0xx_ll_usart.h"
+#include "xprintf.h"
+#include "oled_driver.h"
+
+void drawground();
